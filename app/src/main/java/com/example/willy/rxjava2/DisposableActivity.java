d368418c53bb023242517d07b921f06404bdfdb7
WillyYu <ome450901@gmail.com>
2017-03-07 16:18:18 +0800

    Add disposable useage example

--- app/src/main/java/com/example/willy/rxjava2/DisposableActivity.java
+++ app/src/main/java/com/example/willy/rxjava2/DisposableActivity.java
@@ -10,24 +10,35 @@ import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
-public class MainActivity extends AppCompatActivity {
+public class DisposableActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 檯燈：接收來自開關按鈕的訊息
+        /* 想像是檯燈：接收來自開關按鈕的訊息 */
         Observer<String> observer = new Observer<String>() {
+
+            /*
+                調用dispose後，會切斷檯燈與開關按鈕的聯繫
+                但開關按鈕仍會繼續發送訊息，只是檯燈接收不到
+             */
+            private Disposable mDisposable;
+
             @Override
             public void onSubscribe(Disposable d) {
                 Log.e("test", "onSubscribe");
+                mDisposable = d;
             }
 
             @Override
             public void onNext(String string) {
                 // 收到 開/關 的訊息
                 Log.e("test", "onNext: " + string);
+                if (string.equals("off")) {
+                    mDisposable.dispose();
+                }
             }
 
             @Override
@@ -41,17 +52,19 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        // 開關按鈕：發送 開/關 的訊息給檯燈
+        /* 想像是開關按鈕：發送 開/關 的訊息給檯燈 */
         Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                // 使用 ObservableEmitter 來發送訊息
                 emitter.onNext("on");
                 emitter.onNext("off");
                 emitter.onNext("on");
                 emitter.onComplete();
             }
         });
-        // 把檯燈跟開關串連起來
+
+        // 把檯燈跟開關按鈕串連起來
         observable.subscribe(observer);
 
         // 簡單的寫法，會自動呼叫onComplete
---
+++ app/src/main/java/com/example/willy/rxjava2/NormalActivity.java
@@ -0,0 +1,62 @@
+package com.example.willy.rxjava2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class NormalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* 想像是檯燈：接收來自開關按鈕的訊息 */
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.e("test", "onSubscribe");
+            }
+
+            @Override
+            public void onNext(String string) {
+                // 收到 開/關 的訊息
+                Log.e("test", "onNext: " + string);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Log.e("test", "onError:" + t.getLocalizedMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e("test", "onComplete");
+            }
+        };
+
+        /* 想像是開關按鈕：發送 開/關 的訊息給檯燈 */
+        Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                // 使用 ObservableEmitter 來發送訊息
+                emitter.onNext("on");
+                emitter.onNext("off");
+                emitter.onNext("on");
+                emitter.onComplete();
+            }
+        });
+        
+        // 把檯燈跟開關按鈕串連起來
+        observable.subscribe(observer);
+
+        // 簡單的寫法，會自動呼叫onComplete
+        Observable.just("on", "off", "on").subscribe(observer);
+    }
+}
