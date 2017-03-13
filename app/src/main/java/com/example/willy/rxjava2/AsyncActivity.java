8fb74c5d93ef25996edac2008eb92a63a920a812
willy <willy@mbpr.local>
2017-03-13 11:43:43 +0800

    Adjust method and add comment in AsyncActivitiy

--- app/src/main/java/com/example/willy/rxjava2/AsyncActivity.java
+++ app/src/main/java/com/example/willy/rxjava2/AsyncActivity.java
@@ -1,7 +1,6 @@
 package com.example.willy.rxjava2;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import io.reactivex.Observable;
@@ -11,7 +10,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
-public class AsyncActivity extends AppCompatActivity {
+public class AsyncActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +21,20 @@ public class AsyncActivity extends AppCompatActivity {
     }
 
     private void defaultUsage() {
-        // Consumer 是 Observer 的一種，但他只處理 onNext 訊息，其他訊息都會忽略 (onError, onComplete))
+        /*  Consumer 是 Observer 的一種，但他只處理 onNext 訊息，其他訊息都會忽略 (onError, onComplete)
+            如果只想要處理收到的訊息時，就可以使用Consumer
+         */
         Consumer<String> consumer = new Consumer<String>() {
             @Override
             public void accept(String s) throws Exception {
-                Log.e("test", "Observer thread is : " + Thread.currentThread().getName());
+                Log.e(TAG, "Observer thread is : " + Thread.currentThread().getName());
             }
         };
 
         Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(ObservableEmitter<String> emitter) throws Exception {
-                Log.e("test", "Observable thread is : " + Thread.currentThread().getName());
+                Log.e(TAG, "Observable thread is : " + Thread.currentThread().getName());
 
                 emitter.onNext("on");
                 emitter.onComplete();
@@ -48,14 +49,14 @@ public class AsyncActivity extends AppCompatActivity {
         Consumer<String> consumer = new Consumer<String>() {
             @Override
             public void accept(String s) throws Exception {
-                Log.e("test", "Observer thread is : " + Thread.currentThread().getName());
+                Log.e(TAG, "Observer thread is : " + Thread.currentThread().getName());
             }
         };
 
         Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(ObservableEmitter<String> emitter) throws Exception {
-                Log.e("test", "Observable thread is : " + Thread.currentThread().getName());
+                Log.e(TAG, "Observable thread is : " + Thread.currentThread().getName());
 
                 emitter.onNext("on");
                 emitter.onComplete();
@@ -63,13 +64,25 @@ public class AsyncActivity extends AppCompatActivity {
         });
 
         observable
-                //  'subscribeOn' only the first time is work
+                //  'subscribeOn' 改變 Observer（開關按鈕：發送事件） 的 thread，只有第一次是有用的
                 .subscribeOn(Schedulers.newThread())
-                //  this line will not work
+                //  第二次之後的 'subscribeOn' 都不會發生效用
                 .subscribeOn(AndroidSchedulers.mainThread())
-
-                //  'observeOn' can be called several times
+                //  'observeOn' 改變 Observable（檯燈：接收事件） 的 thread，可以被呼叫多次，並且會在下一行開始有效
                 .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        Log.e(TAG, "After observeOn main thread : " + Thread.currentThread().getName());
+                    }
+                })
+                .observeOn(Schedulers.io())
+                .doOnNext(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        Log.e(TAG, "After observeOn io thread : " + Thread.currentThread().getName());
+                    }
+                })
                 .subscribe(consumer);
     }
 }
